3d2239450c37c454d46c2c37e2ad342fa62e3469
Endrigo Araujo <endrigoaraujo@gmail.com>
2025-02-26 11:04:50 -0300

    Added methods to sql-utils (count and select with offset)

--- sql-utils/sql_utils.go
+++ sql-utils/sql_utils.go
@@ -29,3 +29,13 @@ func CreateUpdateSqlStatement(table string, fields []string, whereClause string,
 	sqlcmd += " RETURNING " + strings.Join(returning, ",")
 	return sqlcmd
 }
+
+func CreateSelectCountSqlStatement(table string) string {
+	return fmt.Sprintf("SELECT COUNT(*) AS total FROM %s", table)
+}
+
+func CreateSelectSqlStatementWithOffset(table string, fields []string, page, size int, orderByColumn, orderByDirection string) string {
+	offset := (page - 1) * size
+	sqlcmd := fmt.Sprintf("SELECT %s FROM %s ORDER BY %s %s LIMIT %d OFFSET %d", strings.Join(fields, ","), table, orderByColumn, orderByDirection, size, offset)
+	return sqlcmd
+}
